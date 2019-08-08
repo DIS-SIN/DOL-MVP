090a2f3423e4af95a8a57a8918842944a29389e7
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-08-08 16:33:21 -0400

    Drop Down Buttons
    The more button can now drop down

--- src/components/Card/InteractionBar.js
+++ src/components/Card/InteractionBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dropdown } from 'react-bootstrap';
 import './InteractionBar.css';
 
 function InteractionBar(props) {
@@ -7,7 +8,16 @@ function InteractionBar(props) {
         <div className="interactionBar">
             <button><span className="icon">endorseOutlined</span> {props.language.endorse}</button>
             <button><span className="icon">commentOutlined</span> {props.language.comment}</button>
-            <button><span className="icon">more</span> {props.language.more}</button>
+            <Dropdown className="dropDownButton">
+                <Dropdown.Toggle variant="success" id="dropdown-basic" className="button">
+                    <span className="icon">more</span> {props.language.more}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
     );
 }
