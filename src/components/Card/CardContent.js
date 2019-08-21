bdbd31facecbdce8bed7d644cf9f76a013ca5848
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-08-21 15:15:40 -0400

    Expanded View Content
    Expanded View now has the ability to recieve props and display data.

--- src/components/Card/CardContent.js
+++ src/components/Card/CardContent.js
@@ -7,7 +7,28 @@ import './CardContent.css';
 function CardContent(props) {
 
     function showExpandedView(){
+        props.setExpandedViewContent({
+            title: props.title,
+            description: props.description
+        });
         props.showExpandedView(true);
+    }
+
+    function compactDescription(description) {
+        let maxDescriptionLength = 270;
+        if (!props.cardViewEnabled){
+            maxDescriptionLength = 78;
+        }
+
+        if (description.length <= maxDescriptionLength){
+            return description;
+        }
+        description = description.substr(0, maxDescriptionLength).split(" ");
+        description.pop();
+
+        description[description.length - 1] = description[description.length - 1].replace(",", "").replace(".", "");
+
+        return `${description.join(" ")}...`;
     }
 
     return (
@@ -17,7 +38,7 @@ function CardContent(props) {
             <ActivityBar language={props.language} endorsements={props.endorsements} comments={props.comments}/>
 
             </div>
-            <p className="cardDescription">{props.description}</p>
+            <p className="cardDescription">{compactDescription(props.description)}</p>
             <Tags language={props.language} difficulty={props.difficulty} timeEstimate={props.timeEstimate}></Tags>
         </div>
     );
--- src/components/expandedView/ExpandedView.js
+++ src/components/expandedView/ExpandedView.js
@@ -4,11 +4,15 @@ import './ExpandedView.css';
 
 function ExpandedView(props) {
 
-    return (
-        <Modal closeTimeoutMS={150} isOpen={props.expandedViewVisible} contentLabel="Expanded View" onRequestClose={props.handleCloseModal} className="Modal" overlayClassName="Overlay">
-            
-        </Modal>
-    );
+    if (props.expandedViewContent != null){
+        return (
+            <Modal closeTimeoutMS={150} isOpen={props.expandedViewVisible} contentLabel="Expanded View" onRequestClose={props.handleCloseModal} className="Modal expandedView" overlayClassName="Overlay">
+                <h1>{props.expandedViewContent.title}</h1>
+                <p>{props.expandedViewContent.description}</p>
+            </Modal>
+        );
+    }
+    return null;
 }
 
 export default ExpandedView;
