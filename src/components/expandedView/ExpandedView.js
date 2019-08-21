4f8c6978e9b5d1d1ee488bba86b0df681d1bd8fa
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-08-21 16:42:17 -0400

    Expanded View Image
    Expanded view now displays the resource thumbnail

--- src/components/expandedView/ExpandedView.js
+++ src/components/expandedView/ExpandedView.js
@@ -10,6 +10,7 @@ function ExpandedView(props) {
     if (props.expandedViewContent != null){
         return (
             <Modal closeTimeoutMS={150} isOpen={props.expandedViewVisible} contentLabel="Expanded View" onRequestClose={props.handleCloseModal} className="Modal expandedView" overlayClassName="Overlay">
+                <img src={props.expandedViewContent.image}></img>
                 <h1>{Title(props.expandedViewContent.title, SpecialTitlePhrases)}</h1>
                 <p>{props.expandedViewContent.description}</p>
             </Modal>
