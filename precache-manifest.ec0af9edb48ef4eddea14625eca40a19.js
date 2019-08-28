d65a9420f60704f08329eb97b7cd7245f2e0c620
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-08-28 16:05:53 -0400

    Updates

---
+++ precache-manifest.ec0af9edb48ef4eddea14625eca40a19.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336ea0a0c067b19fbd26a332c9ba6b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e1e6d539955dfcb2b38",
+    "url": "/static/css/main.4f918522.chunk.css"
+  },
+  {
+    "revision": "c553d9c28b000b1285f1",
+    "url": "/static/js/2.bcfaa2b5.chunk.js"
+  },
+  {
+    "revision": "1e1e6d539955dfcb2b38",
+    "url": "/static/js/main.a4a0bf9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e547f0e949bb2654eaa6826b5968ece",
+    "url": "/static/media/icomoon.0e547f0e.ttf"
+  },
+  {
+    "revision": "2b0ab7c0423e3e97854bfec237b8e918",
+    "url": "/static/media/icomoon.2b0ab7c0.eot"
+  },
+  {
+    "revision": "6c57ef94279ebd4c48af32e9cc9cdf34",
+    "url": "/static/media/icomoon.6c57ef94.woff"
+  },
+  {
+    "revision": "6ed58614a5b5d10fdf6d0595480deba4",
+    "url": "/static/media/icomoon.6ed58614.svg"
+  }
+]);
